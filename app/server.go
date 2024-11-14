f594ce88f6f56ba3c63e83aa0ebc295f8d97cf91
frdslintang <lintangfirdaus2853@gmail.com>
2024-11-14 23:21:28 +0700

    add new file env

--- app/server.go
+++ app/server.go
@@ -3,9 +3,11 @@ package app
 import (
 	"fmt"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Server struct {
@@ -13,8 +15,14 @@ type Server struct {
 	Router *mux.Router
 }
 
-func (server *Server) Initialize() {
-	fmt.Println("Welcome To GoToko")
+type AppConfig struct {
+	AppName string
+	AppEnv  string
+	AppPort string
+}
+
+func (server *Server) Initialize(appConfig AppConfig) {
+	fmt.Println("Welcome to " + appConfig.AppName)
 	server.Router = mux.NewRouter()
 
 	server.initializeRoutes()
@@ -25,8 +33,27 @@ func (server *Server) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, server.Router))
 }
 
+// membaca dan memberikan niai default appname
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+	return fallback
+}
+
 func Run() {
 	var server = Server{}
-	server.Initialize()
-	server.Run(":9000")
+	var appConfig = AppConfig{}
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	appConfig.AppName = getEnv("APP_NAME", "GoToko")
+	appConfig.AppEnv = getEnv("APP_ENV", "development")
+	appConfig.AppPort = getEnv("APP_PORT", "9000")
+
+	server.Initialize(appConfig)
+	server.Run(":" + appConfig.AppPort)
 }
